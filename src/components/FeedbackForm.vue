24f57791aee72a3dfb71fc074dc72984262ecfb6
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-06-08 13:09:50 +0700

    style: prettier FeedbackForm

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -2,8 +2,8 @@
   <div>
     <h1 class="font-bold text-xl mb-2 select-none">Let us know what you think of our product.</h1>
     <div class="flex items-center justify-center mb-2 select-none">
-      <svg class="block text-grey-400 w-4 mr-2" fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <svg class="block text-grey-400 w-4 mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20">
         <path
           d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
       </svg>
