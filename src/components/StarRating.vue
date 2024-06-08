73b3e5533f7a351b53bb78e02af645fa1e90e963
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-06-08 13:36:36 +0700

    feat: emit status based on api response

--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -25,7 +25,7 @@ export default {
   },
   data() {
     return {
-      error: false
+      status: true
     }
   },
   methods: {
@@ -35,7 +35,9 @@ export default {
       try {
         await api.postFeedback({ score: i });
       } catch (error) {
-        this.error = true;
+        this.status = false;
+      } finally {
+        this.$emit('status', this.status);
       }
     }
   }
