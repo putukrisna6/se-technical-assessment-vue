8c78eb20fe7a910802910b2d2a22f06a718052f6
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-06-08 13:36:53 +0700

    feat: display message based on api response

--- src/components/FeedbackModal.vue
+++ src/components/FeedbackModal.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="fixed inset-0 flex items-center justify-center z-50">
+  <div class="fixed inset-0 flex items-center justify-center z-50" @click="closeModal">
     <div class="fixed inset-0 bg-black opacity-50"></div>
-    <div class="relative bg-white rounded-lg shadow-lg p-8 max-w-sm">
+    <div class="relative bg-white rounded-lg shadow-lg p-8 max-w-sm" @click.stop>
       <h2 class="text-xl font-semibold mb-4 text-left">How would you rate your satisfaction with our product?</h2>
       <div class="mt-8 flex justify-center items-center">
-        <StarRating v-model="rating" />
+        <template v-if="status === ''">
+          <StarRating v-model="rating" @status="handleStatus" />
+        </template>
+        <template v-else>
+          <p v-if="status === true" class="text-green-500">Thank you for your feedback!</p>
+          <p v-else class="text-red-500">There was an error submitting your feedback. Please try again.</p>
+        </template>
       </div>
       <div class="flex items-center mt-8">
         <p>Very dissatisfied</p>
         <div class="flex-grow"></div>
         <p>Very satisfied</p>
-      </div>
-      <div class="flex justify-end">
-        <button @click="closeModal" class="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">Close</button>
       </div>
     </div>
   </div>
@@ -24,14 +27,20 @@ import StarRating from "./StarRating.vue";
 export default {
   methods: {
     closeModal() {
-      this.$emit('close');
-    }
+      if (!event.target.closest('.relative')) {
+        this.$emit('close');
+      }
+    },
+    handleStatus(error) {
+      this.status = error;
+    },
   },
   components: {
     StarRating
   },
   data: () => ({
     rating: 4,
+    status: '',
   })
 }
 </script>
